edde5a0f0fc2189c6b1bf675f08765c22554b89f
Kehinde Adejumobi <kehindeadejumobi82@gmail.com>
2026-01-23 14:43:15 +0100

    working on inference engine

--- inference engine/inference.py
+++ inference engine/inference.py
@@ -3,15 +3,49 @@ import os
 from dotenv import load_dotenv
 from jinja2 import Template
 from pathlib import Path
+from langchain_core.pydantic_v1 import BaseModel, Field
+from langchain_openai import ChatOpenAI
+from langchain import globals
+from langchain_core.runnables import chain
+from langchain_core.messages import HumanMessage
+from utils import load_image_chain, get_prompt_from_template
 
+
+class BookInformation(BaseModel):
+    "Information about the book cover image."
+    title: str = Field(..., description="The title of the book.")
+    authors: list[str] = Field(..., description="List of authors of the book.")
+    genre: str = Field(..., description="The genre of the book.")
+    
+class InferenceResponse(BaseModel):
+    "Infer if the book fits the user's preferences."
+    book_info: BookInformation = Field(..., description="Extracted information about the book.")
+    fit_with_preferences: bool = Field(..., description="Whether the book fits the user's preferences.")
+    reason_for_fit: str = Field(..., description="Reason why the book fits the user's preferences.")
 
 class BookInferenceEngine:
     def __init__(self):
         _ = load_dotenv()
         self.client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+        globals.set_debug(True)
         
+    @chain    
     def extract_book_information(self, book_cover):
-        system_template_path = Path("prompts/system_prompt_template.jinja")
-        system_template_content = system_template_path.read_text()
-        system_prompt = Template(system_template_content).render()
+        system_prompt = get_prompt_from_template("prompts/system_prompt_template.jinja")
+        extract_book_information_prompt = get_prompt_from_template("prompts/extract_book_information_template.jinja")
+
+        llm = ChatOpenAI(
+            model="gpt-4o",
+            temperature=0,
+            max_tokens=1024
+        )
+        
+        return llm.invoke([
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": extract_book_information_prompt}
+        ])
+            
+
+        
+        
     
--- inference engine/utils.py
+++ inference engine/utils.py
@@ -1,6 +1,35 @@
 import base64
-def encode_image_to_base64(image_path: str) -> str:
-    with open(image_path, "rb") as image_file:
-        encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
-    return encoded_string
+from langchain.chains import TransformChain
+from pathlib import Path
+from jinja2 import Template
 
+def load_image(inputs: dict) -> dict:
+    """Load an image from the given path and encode it in base64 format."""
+    
+    image_path = inputs.get("image_path")
+    
+    def encode_image(image_path: str) -> str:
+        with open(image_path, "rb") as image_file:
+            encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
+        return encoded_string
+
+    image_base64 = encode_image(image_path)
+
+    return {"image_base64": image_base64}
+
+
+def load_image_chain() -> TransformChain:
+    """Create a TransformChain that loads and encodes an image."""
+    
+    return TransformChain(
+        input_variables=["image_path"],
+        output_variables=["image_base64"],
+        transform=load_image
+    )
+
+
+def get_prompt_from_template(template_path:str) -> str:
+    """Load a prompt template from a file."""
+    path = Path(template_path)
+    content = path.read_text()
+    return Template(content).render()
